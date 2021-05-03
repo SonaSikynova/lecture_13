f153c76a52d0e2ff590650c0031e7856cd5d4b8b
SonaSikynova <sona.sikynova@gmail.com>
2021-05-03 11:01:23 +0200

    Fibonacci.

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursive_nth_fibo(nth):
+    if nth <= 1:
+        return nth
+    else:
+        fibo = recursive_nth_fibo(nth-1) + recursive_nth_fibo(nth-2)
+        return fibo
+
 
 def main():
-    pass
+    nth = int(input("prvok: "))
+    fib_seq = [recursive_nth_fibo(n) for n in range(nth)]
+
+    print(fib_seq)
 
 
 if __name__ == '__main__':
